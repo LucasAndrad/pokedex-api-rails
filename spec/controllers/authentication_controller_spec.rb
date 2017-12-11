1a5c830e07860af3e883b34e60ed460e934c5758
Lucas Andrade <lucasandradeunb@gmail.com>
2017-12-11 17:25:05 -0200

    Updated authentication test with a better sintax

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -4,13 +4,12 @@ include AuthenticationHelper
 RSpec.describe AuthenticationController, type: :controller do
 	describe 'Authentication controller methods' do
 		before(:each) do
-			@user = User.create([
-				{ name: 'Ash Katchum', email: 'ash@email.com', password: '123456', age: 10 }
-			])
+			@user = User.create(name: 'Ash Katchum', email: 'ash@email.com', password: '123456', age: 10)
+			
 		end
 
 		it 'Should authenticate an User' do
-			post :authenticate, params: { 'email' => 'ash@email.com', 'password' => '123456' }
+			post :authenticate, params: { 'email' => @user.email, 'password' => @user.password }
 			json = JSON.parse(response.body)
 			expect(response).to have_http_status(:created)
 			expect(json["auth_token"].length).to be > (20)
