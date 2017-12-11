a2e3f911df72838f9e2e8b4103dc41a844ce90c1
Lucas Andrade <lucasandradeunb@gmail.com>
2017-12-11 11:12:38 -0200

    Created user validations

--- app/models/pokemon_catched.rb
+++ app/models/pokemon_catched.rb
@@ -1,7 +1,11 @@
 class PokemonCatched < ApplicationRecord
 	after_initialize :init
 
+	# Validations
+	validates :level,
+		inclusion: { in: 1..100, message: 'Level %{value} is invalid, must be between 1 and 100' }
+
 	def init
-		self.level = 5
+		self.level = 1
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ApplicationRecord
 	has_secure_password
 	has_many :pokemon_catched
+
+	# Validations
+	validates name:,
+		presence: { message: 'Name is required' }
+
+	# Email
+	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email,
+		presence: { message: 'Email is required' }	
+		uniqueness: {message: 'Email should be unique'},
+		format: {with: VALID_EMAIL, message: 'Use a valid email'}
+	
+	# Password
+	validates password:,
+		presence: { message: 'Password is required' }
+
+	validates_length_of :password,
+    within: 4..30,
+    too_short: 'Password minimum size is 4 characters',
+		too_long: 'Password maximum length is 30 characters'
+		
 end
