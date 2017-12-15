7e66b470c39b748717d2fcafa4e39c55b65a3518
Lucas Andrade <lucasandradeunb@gmail.com>
2017-12-15 16:45:34 -0200

    Create one more test for authentication controller

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -14,5 +14,12 @@ RSpec.describe AuthenticationController, type: :controller do
 			expect(json["auth_token"].length).to be > (20)
 		end
 
+		it 'Should not authenticate an User' do
+			post :authenticate, params: { 'email' => 'email_invalid', 'password' => @user.password }
+			json = JSON.parse(response.body)
+			expect(response).to have_http_status(:unauthorized)
+			expect(json['error']['user_authentication'][0]).to eq('invalid credentials')
+		end
+
 	end
 end
